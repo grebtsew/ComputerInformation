c1d58e7a54cb245423c318d94225325578d4144f
Grebtsew <daniel.grebtsew@gmail.com>
2023-05-21 23:45:58 +0200

    Wrote one actual test, removed old mstest that was for wrong version of .net framework

---
+++ ComputerInformation/ComputerInformation/MSTestComputerInformation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestComputerInformation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestComputerInformation")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4dd7f5c-0141-4ea3-9051-e0ec47edee8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ComputerInformation/ComputerInformation/MSTestComputerInformation/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ComputerInformation;
+
+namespace MSTestComputerInformation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            // Act
+            // Test create form1
+            new Form1();
+            // Assert
+            Assert.IsTrue(true);
+        }
+    }
+}
